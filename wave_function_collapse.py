d708b30732b0ec075d9e9aa283255532d898c7f5
SynnK <93355393+synnkfps@users.noreply.github.com>
2022-09-19 09:56:08 -0300

    rewrite lol

--- wave_function_collapse.py
+++ wave_function_collapse.py
@@ -1,57 +1,90 @@
 import random
 
-#     0    1    2    3    4    5    6    7    8    9    10   11
-c = ["━", "┃", "┏", "┗", "┛", "┓", "┣", "┫", "┳", "┻", "╋", " "]
+#     0   1    2    3    4    5    6    7    8    9    10   11
+c = ["━","┣", "┫", "┳", "┻", " "]
 
 # receive from left/right
-bounding = {
-    c[0]: [c[0], c[4], c[5], c[7], c[8], c[9], c[10]],
-    c[1]: [c[2], c[3], c[6]],
-    c[2]: [c[0], c[4], c[5], c[7], c[8], c[9], c[10]],
-    c[3]: [c[0], c[4], c[5], c[7], c[8], c[9], c[10]],
-    c[4]: [c[2], c[6]],
-    c[5]: [c[2], c[6]],
-    c[6]: [c[0], c[4], c[5], c[8], c[9], c[10]],
-    c[7]: [c[1], c[2], c[3], c[6]],
-    c[8]: [c[0], c[4], c[5], c[7], c[8], c[9], c[10]],
-    c[9]: [c[0], c[4], c[5], c[7], c[8], c[9], c[10]],
-    c[10]: [c[0], c[4], c[5], c[7], c[8], c[9], c[10]],
-    c[11]: [c[1], c[2], c[3], c[6]]
+boundaries = {
+    c[0]: 
+        {
+            "left": [c[0], c[1], c[3], c[4]], 
+            "right": [c[0], c[2], c[3], c[4]]
+        },
+    c[1]:
+        {
+            "left": [c[2], c[5]],
+            "right": [c[0], c[2], c[3], c[4]]
+        },
+    c[2]:
+        {
+            "left": [c[0], c[1], c[3], c[4]],
+            "right": [c[1], c[5]]
+        },
+    c[3]:
+        {
+            "left": [c[0], c[1], c[3], c[4]],
+            "right": [c[0], c[2], c[3], c[4]]
+        },
+    c[4]:
+        {
+            "left": [c[0], c[1], c[3], c[4]],
+            "right": [c[0], c[2], c[3], c[4]]
+        },
+    c[5]:
+        {
+            "left": [c[2], c[5]],
+            "right": [c[1], c[5]]
+        }
 }
 
-output = ''
-count = 0
+#     0   1    2    3    4    5    6    7    8    9    10   11
+c = ["━","┣", "┫", "┳", "┻", " "]
 
-# print(rand, bounding[rand])
-for i in range(20):
-    output += random.choice(c)
-
-def rand_predict(t1, t2):
-    return random.choice(t1[t2])
-
-for j in output:
-    index = output.index(j)
-    current = output[index]
-    before = output[index-1] if index > 0 else ''
-    
-    if before != '' and current in bounding[before]:
-        pass
-    elif before != '' and current not in bounding[before]:
-        random_predict = rand_predict(bounding,before)
-
-        print(f'Changing: "{output[index]}[{index}]" with "{random_predict}" because it isnt compatible with {before}')
-        output = list(output)
-        output[index] = random_predict
-        output = ''.join(output)
-
-    print(f'\tBefore: {before}\n\tCurrent: {output[index]}')
-
-print(output)
+##     0    1    2    3    4    5    6    7    8    9    10   11
+#c = ["━", "┃", "┏", "┗", "┛", "┓", "┣", "┫", "┳", "┻", "╋", " "]
 
 
+output = []
 
-# easier way to get a char before another
+for i in range(2):
+    rand1 = random.choice(c)
+    rand2 = random.choice(c)
 
-# string = 'abcdef'
-# print(string[string.index('c')-1])
+    if rand1 in boundaries[rand2][random.choice(['left'])]:
+        output.append(rand1)
+        output.append(rand2)
+    else:
+        rand1 = random.choice(c)
+        rand2 = random.choice(c)
 
+    if rand1 in boundaries[rand2][random.choice(['right'])]:
+        output.append(rand2)
+        output.append(rand1)
+    else:
+        rand2 = random.choice(c)
+        rand1 = random.choice(c)
+
+# Corrigindo
+for i in range(len(output)):
+    for k, v in enumerate(output):
+        try:
+            if output[k] in boundaries[output[k+1]]['left']:
+                #print(f'{output[k]} bounds with {output[k+1]} from left ({output[k]}{output[k+1]})')
+                pass
+            else:
+                rLeft = random.choice(boundaries[output[k+1]]['left'])
+                print(f'(left) changing {output[k]} to {rLeft} so it can bound to {output[k+1]} ({rLeft}{output[k+1]})')
+                output[k] = rLeft
+
+            if output[k] in boundaries[output[k-1]]['right']:
+                #print(f'{output[k]} bounds with {output[k-1]} from right({output[k-1]}{output[k]})')
+                pass
+            else:
+                rRight = random.choice(boundaries[output[k-1]]['right'])
+                print(f'(right) changing {output[k]} to {rRight} so it can bound to {output[k-1]} ({output[k-1]}{rRight})')
+                output[k] = rRight
+        except:
+            pass
+
+print('')
+print(''.join(output))
